5c0afe7e8a91d39d2803fa999505d9bb2a4b3a38
Gustav Gränsbo <gusgr644@student.liu.se>
2019-09-15 15:37:21 +0200

    Add suggested tests for BBCReader

--- aiconf/reader_test.py
+++ aiconf/reader_test.py
@@ -7,12 +7,35 @@ from aiconf.reader import BBCReader
 
 FIXTURES_ROOT = pathlib.Path(__file__).parent / 'fixtures'
 
+CATEGORIES = {'tech', 'sport', 'politics', 'entertainment', 'business'}
 
 class ReaderTest(AllenNlpTestCase):
-    def test_reader(self):
+    def test_n_instances(self):
         token_indexers = {"tokens": SingleIdTokenIndexer()}
         reader = BBCReader(token_indexers)
         instances = reader.read(str(FIXTURES_ROOT / 'tiny.csv'))
+
+        assert len(instances) == 5
+
+    def test_labels(self):
+        token_indexers = {"tokens": SingleIdTokenIndexer()}
+        reader = BBCReader(token_indexers)
+        instances = reader.read(str(FIXTURES_ROOT / 'tiny.csv'))
+
+        assert not set(instance['category'].label for instance in instances).difference(CATEGORIES)
+
+    def test_instance_values(self):
+        token_indexers = {"tokens": SingleIdTokenIndexer()}
+        reader = BBCReader(token_indexers)
+        instances = reader.read(str(FIXTURES_ROOT / 'tiny.csv'))
+        first_instance = instances[0]
+        assert [token.text for token in first_instance['text'].tokens] == [
+            "Computer", "grid", "to", "help", "the", "world", 
+            "Your", "computer", "can", "now", "help", "solve",
+            "the", "worlds", "most", "difficult", "health", "and", "social", "problems"
+        ]
+
+        assert first_instance['category'].label == 'tech'
 
         # Some ideas of things to test:
         # * test that there are 5 instances
